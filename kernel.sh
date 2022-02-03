8f9215197378ff0571ee245ca7088e4b3e5e5c1b
Mayank Gajbhiye <mayank1398@gmail.com>
2022-02-03 21:57:54 +0530

    fixed typo

--- kernel.sh
+++ kernel.sh
@@ -72,7 +72,7 @@ DEFCONFIG=vendor/violet-perf_defconfig
 COMPILER=clang
 
 # Build modules. 0 = NO | 1 = YES
-MODULES=0
+MODULES=1
 
 # Specify linker.
 # 'ld.lld'(default)
@@ -198,7 +198,7 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 	fi
 
 	msg "|| Cloning Anykernel ||"
-	git clone --depth 1 --no-single-branch https://github.com/"$AUTHOR"/AnyKernel3.git
+	git clone --depth 1 --no-single-branch https://github.com/MecloV/AnyKernel3.git
 
 	if [ $BUILD_DTBO = 1 ]
 	then
